32f618b9df4c00fdaa5e5b7e9b149568a65ccab0
walkwel-dev <wdev969@gmail.com>
2025-12-28 18:43:21 +0530

    update index

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
+
+app.use(cors());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Server is UP" });
@@ -11,6 +14,32 @@ app.get("/api/health", (req, res) => {
 
 app.get("/api/hello", (req, res) => {
   res.status(200).json({ success: true, message: "Hello from server" });
+});
+
+app.get("/api/fun", async (req, res) => {
+  const { type } = req.query;
+
+  try {
+    let data;
+
+    if (type === "joke") {
+      const response = await fetch(
+        "https://v2.jokeapi.dev/joke/Any?type=single"
+      );
+      const result = await response.json();
+      data = result.joke;
+    }
+
+    if (type === "advice") {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const result = await response.json();
+      data = result.slip.advice;
+    }
+
+    res.json({ success: true, data });
+  } catch (err) {
+    res.status(500).json({ success: false, error: "Something went wrong" });
+  }
 });
 
 app.listen(PORT, () => {
